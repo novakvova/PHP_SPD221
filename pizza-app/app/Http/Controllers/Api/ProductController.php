2eff3139a3958ee46489b24da85ba4fc9e6b164a
novakvova <novakvova@gmail.com>
2024-07-06 17:02:18 +0300

    Working app

--- pizza-app/app/Http/Controllers/Api/ProductController.php
+++ pizza-app/app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\ProductImage;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -13,39 +14,55 @@ use Intervention\Image\ImageManager;
 class ProductController extends Controller
 {
     protected string $upload;
-    protected array $sizes = [50,150,300,600,1200];
+    protected array $sizes = [50, 150, 300, 600, 1200];
 
     public function __construct()
     {
-        $this->upload=env("UPLOAD_DIR");
+        $this->upload = env("UPLOAD_DIR");
     }
+
     public function getAll(Request $request): \Illuminate\Http\JsonResponse
     {
-        $id=$request->query("categoryId");
-        $items = Product::with(["category","product_images"])
-            ->where("category_id", "=",$id)->get();
-        return response()->json($items) ->header('Content-Type', 'application/json; charset=utf-8');
+        $id = $request->query("categoryId");
+        $items = Product::with(["category", "product_images"])
+            ->where("category_id", "=", $id)->get();
+        return response()->json($items)->header('Content-Type', 'application/json; charset=utf-8');
     }
 
-    public function store(Request $request) {
-        $dir = public_path($this->upload);
-        $images = $request->file("images");
-//        if($request->hasFile('images') && $request->input("name")!="") {
-//            $file = $request->file("image");
-//            $fileName = $this->saveImage($file);
-//            $item = Product::create(['name'=>$request->input('name'), "image"=> $fileName]);
-//            return response()->json($item,201);
-//        }
-        return response()->json("Bad request", 400);
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+        $product = Product::create($validatedData);
+
+        if ($request->hasFile('images')) {
+            $images = $request->file("images");
+            $i = 0;
+            foreach ($images as $file) {
+                $fileName = $this->saveImage($file);
+                ProductImage::create([
+                    'name' => $fileName,
+                    'priority' => $i++, // You can adjust this as needed
+                    'product_id' => $product->id,
+                ]);
+            }
+        }
+        return response()->json($product, 201);
     }
 
-    protected function saveImage(UploadedFile $file) {
-        $fileName = uniqid(). ".webp";
+    protected function saveImage(UploadedFile $file)
+    {
+        $fileName = uniqid() . ".webp";
         $manager = new ImageManager(new Driver());
         foreach ($this->sizes as $size) {
             $imageSave = $manager->read($file);
             $imageSave->scale(width: $size);
-            $path = public_path($this->upload.$size."_".$fileName);
+            $path = public_path($this->upload . $size . "_" . $fileName);
             $imageSave->toWebp()->save($path);
         }
         return $fileName;
